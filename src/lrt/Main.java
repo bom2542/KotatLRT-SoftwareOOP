d2f1c41cde5e4922349df02c096de7d10d92e506
bom2542 <61776315+bom2542@users.noreply.github.com>
2020-10-06 17:14:03 +0700

    Commit

--- src/lrt/Main.java
+++
@@ -1,9 +0,0 @@
-package lrt;
-
-public class Main() {
-	public static void main(String[] args) {
-		System.out.println("Suranaree University Of Technogy");
-		System.out.printf("System Opraration Online");
-		System.out.println("Hello 555");
-	}
-}
---
+++ src/lrt/Main2.java
@@ -0,0 +1,7 @@
+package lrt;
+
+public class Main2 {
+	public static void(String args[]) {
+		
+	}
+}
