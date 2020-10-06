44fe9f620b5fa1b417b2a85d1859cd99b17b684b
chidchansa <71626709+chidchansa@users.noreply.github.com>
2020-10-06 16:46:59 +0700

    init Commit

--- src/lrt/Main.java
+++ src/lrt/Main.java
@@ -4,6 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Suranaree University Of Technogy");
 		System.out.printf("System Opraration Online");
-		System.out.println("why this is not update");
+		System.out.println("Hello 555");
 	}
 }
