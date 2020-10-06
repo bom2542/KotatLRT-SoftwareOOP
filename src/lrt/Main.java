f4c8fcd99375ccbc2966fbeb21803c6b4c1ef7dd
tachin0071 <51067862+tachin0071@users.noreply.github.com>
2020-10-06 15:06:59 +0700

    Update Main.java

--- src/lrt/Main.java
+++ src/lrt/Main.java
@@ -3,5 +3,6 @@ package lrt;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Suranaree University Of Technogy");
+		System.out.printf("System Opraration Online");
 	}
 }
